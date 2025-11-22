b0d398046add2eb74401592c589b637359b96d99
alkonosst <m.ramirez.bravo.94@gmail.com>
2025-11-22 01:01:58 -0300

    refactor: Change library license, add funding file, bump version

--- examples/Interrupts/Interrupts.ino
+++ examples/Interrupts/Interrupts.ino
@@ -1,7 +1,7 @@
 /**
  * SPDX-FileCopyrightText: 2025 Maximiliano Ramirez <maximiliano.ramirezbravo@gmail.com>
  *
- * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-License-Identifier: MIT
  */
 
 // Uncomment the following line if you want to use custom callbacks
